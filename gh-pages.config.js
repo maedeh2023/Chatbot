fb0be0570efe8931941769035f68adf33c3bedeb
Maedeh2023 <maedehjalilnasab@gmail.com>
2025-01-09 22:19:31 -0400

    initial setup

---
+++ gh-pages.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branch: 'main',
+    repo: 'https://github.com/your-username/your-repo-name.git',
+    user: {
+      name: 'Your Name',
+      email: 'your-email@example.com'
+    }
+  };
